464b0ad41a65eb6e85ae1584f714c66f09064c0e
Peter Hu <peikang.hu@gmail.com>
2017-01-09 22:53:10 -0600

    + Added Login

--- ASPCoreSportsStore/src/ASPCoreSportsStore/Controllers/AccountController.cs
+++ ASPCoreSportsStore/src/ASPCoreSportsStore/Controllers/AccountController.cs
@@ -30,9 +30,34 @@ namespace ASPCoreSportsStore.Controllers
 			return View(new LoginModel{ ReturnUrl = returnUrl });
 		}
 
-		public IActionResult Index()
-        {
-            return View();
-        }
+		[HttpPost]
+		[AllowAnonymous]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login(LoginModel loginModel)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await userManager.FindByNameAsync(loginModel.Name);
+
+				if (user != null)
+				{
+					await signInManager.SignOutAsync();
+
+					if ((await signInManager.PasswordSignInAsync(user, loginModel.Password, false, false)).Succeeded)
+					{
+						return Redirect(loginModel?.ReturnUrl ?? "/Admin/Index");
+					}
+				}
+			}
+
+			ModelState.AddModelError("", "Invalid name or password");
+			return View(loginModel);
+		}
+
+		public async Task<RedirectResult> Logout(string returnUrl = "/")
+		{
+			await signInManager.SignOutAsync();
+			return Redirect(returnUrl);
+		}
     }
 }
